a9dd3b1e71d7b29d3f76ec212393661ec9a02eda
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-11-14 21:53:36 +0530

    Added Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,22 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { SubmissionsModule } from './submissions/submissions.module';
 import { AuthModule } from './auth/auth.module';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
-import { UserModule } from './user/user.module';
-import { ContestantModule } from './contestant/contestant.module';
 import { ProblemsModule } from './problems/problems.module';
+import { PrismaService } from './prisma-service/prisma.service';
 
 @Module({
   imports: [
     SubmissionsModule,
     AuthModule,
     LeaderboardModule,
-    UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_URI'),
-      }),
-    }),
-    ContestantModule,
     ProblemsModule,
   ],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
